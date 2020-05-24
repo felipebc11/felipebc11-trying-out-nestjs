f86ea08acc0cd9cb0c831b614e839a185e9bf78e
felipebc11 <felipebc11@gmail.com>
2020-05-24 12:38:45 -0300

    CRUD of persons finalized.

--- src/person/person.controller.ts
+++ src/person/person.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Patch, Delete } from '@nestjs/common';
 
-@Controller('person')
-export class PersonController {}
+import { PersonService} from './person.service';
+
+@Controller('persons')
+export class PersonController {
+  constructor(private readonly personsService : PersonService) {}
+
+  @Post('create')
+  async addPerson(
+    @Body('name') name: string,
+    @Body('email') email: string
+  ) {
+    const generateId = await this.personsService.create(
+      name,
+      email
+    );
+    return {id: generateId._id};
+  }
+
+  @Get()
+  async getAll(){
+    return this.personsService.fetchAll();
+  }
+
+  @Get('findById/:id')
+  async findOne(
+    @Param('id') userId: string,
+  ){
+    return this.personsService.findOne(userId);
+  }
+
+  @Patch()
+  async updatePerson(
+    @Body('personId') personId: string,
+    @Body('name') name: string,
+    @Body('email') email: string,
+    @Body('friend') friend: string,
+  ){
+    return this.personsService.updatePerson(personId, name, email, friend);
+  }
+
+  @Delete('delete/:id')
+  async deletePerson(
+    @Param('id') personId: string,
+  ){
+    return this.personsService.removePerson(personId);
+  }
+}
--- src/person/person.model.ts
+++ src/person/person.model.ts
@@ -1,1 +1,17 @@
-export class PersonModel {}
+import * as mongoose from 'mongoose';
+
+export const PersonSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  friend: {
+    type: String,
+    required: false
+  }
+});
