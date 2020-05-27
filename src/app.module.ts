b5da625091a9626514564c3bd8787049e5227119
felipebc11 <felipebc11@gmail.com>
2020-05-26 21:52:07 -0300

    The raffle system and the email service were implemented.

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,16 +7,19 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PersonModule } from './person/person.module';
 import { AuthModule } from './auth/auth.module';
+import { DrawModule } from './draw/draw.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     UserModule,
     AuthModule,
+    DrawModule,
     MongooseModule.forRoot(
       `mongodb://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@localhost:27017`,
     ),
-    PersonModule
+    PersonModule,
+    
   ],
   controllers: [AppController],
   providers: [AppService],
